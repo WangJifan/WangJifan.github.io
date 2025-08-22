f80f8817d84e348379012e716d28f0f670fe675b
WangJifan <85206581+WangJifan@users.noreply.github.com>
2025-08-22 01:41:29 +0000

    Deploying to gh-pages from @ WangJifan/WangJifan.github.io@98a3fe4ac66703d113d59e9932a8372e785c3382 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,13 +94,6 @@ ninja.data = [{
           title: 'Started my PhD program in database systems at UW–Madison!',
           description: "",
           section: "News",},{
-        id: 'social-bluesky',
-        title: 'Bluesky',
-        section: 'Socials',
-        handler: () => {
-          window.open("bsky.app/profile/abigalekim.bsky.social", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -120,13 +113,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/jifanwang", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
